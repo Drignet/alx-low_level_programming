2971e3cf9263a35efa4b1099a086eedfcfb5171e
Drignet <odibehenry@gmail.com>
2022-06-02 18:15:58 -0700

    k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
-/*my main first c programme*/
+
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 
 return (0);
 }
