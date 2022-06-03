51411e883c95b87287ea518c5c740e37b5647151
Drignet <odibehenry@gmail.com>
2022-06-02 19:44:57 -0700

    l

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ int i;
 long int li;
 long long int lli;
 float f;
-printf("Size of char: %zu bytes\n", sizeof(c));
-printf("Size of int: %zu bytes\n", sizeof(i));
-printf("Size of long int: %zu bytes\n", sizeof(li));
-printf("Size of long long int: %zu bytes\n", sizeof(lli));
-printf("Size of float: %zu bytes\n", sizeof(f));
+printf("Size of a char: %zu byte(s)\n", sizeof(c));
+printf("Size of an int: %zu byte(s)\n", sizeof(i));
+printf("Size of a long int: %zu byte(s)\n", sizeof(li));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(lli));
+printf("Size of a float: %zu byte(s)\n", sizeof(f));
 
 return (0);
 }
