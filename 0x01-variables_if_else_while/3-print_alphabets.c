c3975cdaa6b4f607d42154a1e348e719091aecb4
Drignet <odibehenry@gmail.com>
2022-06-03 17:35:20 -0700

    update

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,12 +10,15 @@
 
 int main(void)
 {
-for (char i = 'a'; i <= 'z'; i++)
+char i;
+char j;
+ 
+for (i = 'a'; i <= 'z'; i++)
 {
 putchar(i);
 }
 
-for (char j = 'A'; j <= 'Z'; j++)
+for (j = 'A'; j <= 'Z'; j++)
 {
 putchar(j);
 }
