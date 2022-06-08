2abc566f8da7d82318059f54c03121152b76a719
Drignet <odibehenry@gmail.com>
2022-06-08 15:39:46 -0700

    natural number from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_to_98 - function that prints from n - 98
+ *
+ * @n: The initial number
+ * Return: n to 98 natural numbers
+ *
+ */
+
+void print_to_98(int n)
+{
+int n, i;
+for (i = n; i <= 98; i++)
+_putchar(i + '0');
+}
