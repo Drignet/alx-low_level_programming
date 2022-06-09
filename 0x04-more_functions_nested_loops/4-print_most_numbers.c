40ede33e27eaeb8c1a1b31ef4494cbdb8e3a5c96
Drignet <odibehenry@gmail.com>
2022-06-09 16:15:35 -0700

    function that prints most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that print most numbers
+ * Return: most numbers
+ *
+ */
+
+void print_most_numbers(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+if ((n != 2) && (n != 4))
+{
+_putchar(n + '0');
+}
+}
+_putchar('\n');
+}
