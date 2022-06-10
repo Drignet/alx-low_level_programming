54f1a4edd5a303a44a728784bde6b54f7b2d448e
Drignet <odibehenry@gmail.com>
2022-06-09 18:08:48 -0700

    draw line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,16 +10,18 @@
 
 void print_line(int n)
 {
-if (n <= 0)
-{
-_putchar('\n');
-}
-else
-{
-for (n = 0; n < 10; n++)
-{
-_putchar('_');
-}
-}
-_putchar('\n');
+	int co;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (co = 1; co <= n; co++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
