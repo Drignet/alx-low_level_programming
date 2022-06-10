180ebf6ddeb359f579b0f7f0246d2e511608a8bf
Drignet <odibehenry@gmail.com>
2022-06-09 17:15:45 -0700

    update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,15 +10,15 @@ void more_numbers(void)
 {
 int m, n;
 for (m = 0; m < 10; m++)
-{
+{i
 for (n = 0; n < 15; n++)
 {
 if (n >= 10)
 {
 _putchar((n / 10) + '0');
 }
-_putchar((n % 10) + 0);
+_putchar((n % 10) + '0');
+}
 }
 _putchar('\n');
-}
 }
