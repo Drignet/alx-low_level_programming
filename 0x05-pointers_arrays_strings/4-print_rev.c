a64025e7f44b8e44e7510ef5dddde1fc3b561208
Drignet <odibehenry@gmail.com>
2022-06-13 16:22:49 -0700

    reverse strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,21 +1,18 @@
 #include "main.h"
 
 /**
- * rev_string - Reverses a string.
- * @s: string to be reserved.
+ * print_rev - Prints a string in reverse.
+ * @s: string to tbe reserved.
  */
-void rev_string(char *s)
+void print_rev(char *s)
 {
-	int i = 0, len = 0;
-	char tmp;
+	int len = 0, i = 0;
 
 	while (s[i++])
 		len++;
 
-	for (i = len - 1; i >= len / 2; i--)
-	{
-		tmp = s[i];
-		s[i] = s[len - i - 1];
-		s[len - i - 1] = tmp;
-	}
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ void swap_int(int *a, int *b);
 size_t _strlen(const char *s);
 void _puts(char *str);
 void print_rev(char *s);
+void rev_string(char *s);
 
 
 #endif /* main_h*/
