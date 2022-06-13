e62f665a5c5a75f2266a226fa4fcfc4c6c2f4414
Drignet <odibehenry@gmail.com>
2022-06-13 13:56:42 -0700

    points to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,15 +4,11 @@
  * reset_to_98 - function that takes a pointer to an int
  * as parameter and update to ,98
  *@*n: pointer
- *Return:value 98
+ *Return:void
  *
  */
 
 void reset_to_98(int *n)
 {
-int n;
-int *pn = &n;
-
-n = 98;
-printf("%d", *pn);
+*n = 98;
 }
