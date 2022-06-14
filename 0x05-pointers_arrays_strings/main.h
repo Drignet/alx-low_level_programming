cbd4f3661997906f9aab7c37878c865be7a5286e
Drignet <odibehenry@gmail.com>
2022-06-13 17:07:06 -0700

    convert string to integer

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -14,6 +14,7 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 
 #endif /* main_h*/
