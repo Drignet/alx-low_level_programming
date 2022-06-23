8a07123aad06080c850de61c0c7aba09ddeb8f66
Drignet <odibehenry@gmail.com>
2022-06-22 18:06:52 -0700

    m

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _pow_recursion - function that returns the value of x raised to the power of y
+ * _pow_recursion - function that return value of x raised to the power of y
  *
  * @x: Base
  * @y: power
